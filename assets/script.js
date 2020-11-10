d173788ddddc7027f081eff13a4a4388a4a70b17
DimitriNakos <dimi1nakos@gmail.com>
2020-11-09 23:48:04 -0500

    HTML structured and jQuery values determined

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,15 @@
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'))
+
+let hour = parseInt(moment().format("HH"))
+
+for (let i = 0; i < 25; i++) {
+    if (i > hour) {
+        $("#" + i).addClass("future")
+    } else if (i < hour) {
+        $("#" + i).addClass("past")
+    } else if (i === hour) {
+        $("#" + i).addClass("present")
+    }
+}
+
+
