bf447ccd9d21878ecfcf0a2e9407aec4b5d0cd64
DimitriNakos <dimi1nakos@gmail.com>
2020-11-11 00:07:22 -0500

    added localstorage and corrected some old logic

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,26 @@ $("#currentDay").text(moment().format('dddd MMMM Do YYYY'))
 
 let hour = parseInt(moment().format("HH"))
 
-for (let i = 0; i < 25; i++) {
+for (let i = 9; i < 18; i++) {
     if (i > hour) {
         $("#" + i).addClass("future")
     } else if (i < hour) {
         $("#" + i).addClass("past")
-    } else if (i === hour) {
+    } else {
         $("#" + i).addClass("present")
+    }
+    var localStore = localStorage.getItem("hour" + i)
+    if (localStore !== null) {
+        $("#text_" + i).val(localStore)
     }
 }
 
+for (let i = 9; i < 18; i++) {
+    var textInput = $("#text_" + i)
+}
 
+$(".saveBtn").on("click", function () {
+    var clicked = $(this).attr("data-timing")
+    var text = $("#text_" + clicked).val()
+    localStorage.setItem("hour" + clicked, text)
+})
